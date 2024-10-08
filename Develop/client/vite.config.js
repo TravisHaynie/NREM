ae40ab975001e141db1653c2c7833d0491a795c5
unknown <travihay@gmail.com>
2024-10-08 18:32:40 -0500

    changed vite

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL || 'http://localhost:5000', // Proxy API requests
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
