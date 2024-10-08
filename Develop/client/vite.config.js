7a25af48a83ba2a6281d56645bf5804dfffe794f
unknown <travihay@gmail.com>
2024-10-08 17:41:28 -0500

    downloaded dep

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: import.meta.env.VITE_API_URL,  // Proxy to the backend API
         secure: false,
-        changeOrigin: true
-      }
-    }
+        changeOrigin: true,
+      },
+    },
   },
-  define: {
-    'process.env': process.env
-  }
-})
+});
