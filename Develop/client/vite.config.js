910f070cf9822c5dd4e601cebf842c4e691a49e7
unknown <travihay@gmail.com>
2024-10-08 18:02:56 -0500

    change vite

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 
-// Load environment variables from the .env file
-dotenv.config();
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,  // Use process.env instead of import.meta.env
-        secure: false,
+        target: 'http://localhost:3001',  // This should be your API backend URL during development
         changeOrigin: true,
+        secure: false,
       },
     },
   },
