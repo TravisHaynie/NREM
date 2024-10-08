9e2a1375e4aeeccffeba8aaf20907249e77c3599
unknown <travihay@gmail.com>
2024-10-08 18:40:11 -0500

    added code

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',  // This should be your API backend URL during development
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -14,6 +14,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 app.use(routes);
 
 db.once('open', () => {
