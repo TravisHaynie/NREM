d4e19a3c9b4a3dc5bd2c569f133d857c7086c457
unknown <travihay@gmail.com>
2024-10-08 19:13:34 -0500

    changed server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -9,19 +9,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve static assets from the client/dist folder in production
+// Serve static assets from the correct folder
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // If no API routes are hit, send the React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html')); 
   });
 }
 
 // Use API routes
 app.use(routes);
 
+// Start the server
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
