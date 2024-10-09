bf472b7528a93d6a8fb25cf426e2428ec208df8a
unknown <travihay@gmail.com>
2024-10-08 19:04:02 -0500

    changed server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 // Serve static assets from the client/dist folder in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/dist')));
+  app.use(express.static(path.join(__dirname, './client/dist')));
 
   // If no API routes are hit, send the React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.join(__dirname, './client/dist/index.html'));
   });
 }
 
